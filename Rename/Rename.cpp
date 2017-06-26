cd75bdb7eaf452ef5ce66ce63a35ac8671da0aef
M1p0 <lx669196851@gmail.com>
2017-06-26 13:39:10 +0800

    Update Rename.cpp

--- Rename/Rename.cpp
+++ Rename/Rename.cpp
@@ -7,29 +7,30 @@ Order order;
 
 int main()
 {
-	string szPath;
-	string szTemp;
-	string ending;
-	string oldname;
-	string newname;
-	cout<<"文件地址:"<<endl;
-	cin>>szPath;
+    string szPath;
+    string szTemp;
+    string ending;
+    string oldname;
+    string newname;
+    cout<<"文件地址:"<<endl;
+    cin>>szPath;
 
-	szTemp=szPath+"\\*.*";
+    szTemp=szPath+"\\*.*";
 
-	ListDir(szTemp.c_str());
+    ListDir(szTemp.c_str());
 
-	cout<<"文件类型:"<<endl;
-	cin>>ending;
-	szPath=szPath+"\\";
+    cout<<"文件类型:"<<endl;
+    cin>>ending;
+    szPath=szPath+"\\";
 
-	for (int i = 0; i < order.length; ++i)
-	{
-		string temp;
-		temp=to_string(i);
-		oldname=szPath+order.name[i];
-		newname=szPath+temp+"."+ending;
-	}
+    for (int i = 0; i < order.length; ++i)
+    {
+        string temp;
+        temp=to_string(i);
+        oldname=szPath+order.name[i];
+        newname=szPath+temp+"."+ending;
+        rename(oldname.c_str(),newname.c_str());
+    }
 
 
 }
