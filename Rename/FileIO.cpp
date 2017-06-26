30d1a25b39bd24639e966928b66170446560334c
M1p0 <lx669196851@gmail.com>
2017-06-26 13:22:10 +0800

    Update FileIO.cpp

--- Rename/FileIO.cpp
+++ Rename/FileIO.cpp
@@ -1,156 +1,30 @@
 #include "FileIO.h"
-
+#include <string>
 #include <iostream>
 
 using namespace std;
 
+extern order;
 
+int i=0;
 
-void FileIO::Create(string location)
-
-{
-
-    ofstream CreateFile(location);
-
-    CreateFile.close();
-
-}
-
-
-
-void FileIO::Write(string location, string data)
-
-{
-
-    ofstream WriteFile(location, ios::app);
-
-    WriteFile << data;
-
-    WriteFile.close();
-
-}
-
-
-
-void FileIO::Read(string location)
-
-{
-
-    char buffer[256];
-
-    ifstream ReadFile(location);
-
-    if (!ReadFile.is_open())
-
-    {
-
-        cout << "Failed to Read the file" << endl;
-
-        return;
-
-    }
-
-    while (!ReadFile.eof())                //或cout<<ReadFile.rdbuf()
-
-    {
-
-        ReadFile.getline(buffer, 100);
-
-        cout << buffer << endl;
-
-    }
-
-
-
-}
-
-
-
-
-
-void FileIO::Copy(string SourceFile, string NewFile)
-
-{
-
-    ifstream in;
-
-    ofstream out;
-
-    in.open(SourceFile.c_str(), ios::binary);
-
-    if (in.fail())
-
-    {
-
-        cout << "Failed to open source file" << endl;
-
-        in.close();
-
-        out.close();
-
-        return;
-
-    }
-
-    out.open(NewFile.c_str(), ios::binary);
-
-    if (out.fail())
-
-    {
-
-        cout << "Failed to create new file" << endl;
-
-        in.close();
-
-        out.close();
-
-        return;
-
-    }
-
-    else
-
-    {
-
-        out << in.rdbuf();
-
-        out.close();
-
-        in.close();
-
-        return;
-
-    }
-
-
-
-}
-
-
-
-
-
-
-
-void FileIO::ListDir(LPCTSTR lpFileName)
+void FileIO::ListDir(LPCTSTR lpFileName) //非远库中函数 已经过修改
 
 {
 
     WIN32_FIND_DATA FileData;
-
     HANDLE hSearch;
-
-
-
     hSearch = FindFirstFile(lpFileName, &FileData);
 
     while (FindNextFile(hSearch, &FileData))
-
     {
-
-        cout << "得到文件：" << FileData.cFileName << endl;
-
-        //cout << FileData.dwFileAttributes << endl; //若为文件夹 则数值为16
+        if(FileData.dwFileAttributes==16)
+            continue;
+        else
+        {
+            order.name[i]=FileData.cFileName;
+            ++i;
+        }
 
     }
 
