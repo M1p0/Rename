255bb81854fad670cd03bce819c6c4abbee8fd59
M1p0 <lx669196851@gmail.com>
2017-06-26 13:56:14 +0800

    Update FileIO.h

--- Rename/FileIO.h
+++ Rename/FileIO.h
@@ -6,16 +6,7 @@
 using namespace std;
 
 class FileIO
-
 {
-
 public:
-    void Create(string location);//例如File.Create("c:\\test.txt")
-    void Write(string location, string data);
-    void Read(string location);
-    void Copy(string SourceFile, string NewFile);
     void ListDir(LPCTSTR lpFileName);
-    void Backup();
-    void CopyFolder(string szPath, string szTarget);
-
 };
