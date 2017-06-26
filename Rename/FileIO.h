0c8383d33bc924f07c96333705a75b48c5860851
M1p0 <lx669196851@gmail.com>
2017-06-26 12:49:41 +0800

    Update FileIO.h

--- Rename/FileIO.h
+++ Rename/FileIO.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+
+class FileIO
+{
+public:
+    void Create(string location);//例如File.Create("c:\\test.txt")
+    void Write(string location, string data);
+    void Read(string location);
+    void Copy(string SourceFile, string NewFile);
+    void ListDir(LPCTSTR lpFileName);
+    void Backup();
+    void CopyFolder(string szPath, string szTarget);
+};
